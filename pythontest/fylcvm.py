b854a0098d385fc22740ccb8eabede4b4a9d0be1
cht265358 <tba9jt@virginia.edu>
2024-10-06 11:07:29 -0400

    Add files via upload

--- pythontest/fylcvm.py
+++ pythontest/fylcvm.py
@@ -39,7 +39,7 @@ class FYLCVM:       #base class for FCC
             return 1;
 
     def compute_basic_cluster_prob(self,partition,site_potential):
-        spm=np.append(np.zeros(self.clustersize),site_potential)
+        spm=np.append(np.zeros(self.clustersize),site_potential)               #spm is site potential matrix
         basic_cluster_prob=np.zeros(self.shape)
         for i,j,k,l in itertools.product(range(self.component), repeat=4):
             basic_cluster_prob[i][j][k][l]=np.exp(spm[i][0]+spm[j][1]+spm[k][2]+spm[l][3]-self.basicclusterenergy[i][j][k][l])/partition
@@ -48,7 +48,7 @@ class FYLCVM:       #base class for FCC
         partition=self.compute_partition_function(site_potential)
         #two body cluster
         two_body_energy=0
-        for i in range(self.clustersize):
+        for i in range(self.clustersize):                 #4*3*2*2 two body cluster
             for j in range(i+1,self.clustersize):
                 for k,l in itertools.product(range(self.component), repeat=2):     #decorate this line later
                     position_type_matrix=np.zeros((2,2))
@@ -57,33 +57,16 @@ class FYLCVM:       #base class for FCC
                     position_type_matrix[1][0]=k
                     position_type_matrix[1][1]=l
                     probij=utility.get_cluster_prob(prob,position_type_matrix)
-        two_body_energy+=probij*np.log(probij)*T
+                    two_body_energy+=probij*np.log(probij)*T
         pointenergy=0
         point_prob=np.zeros(self.maxsize)
-        for i in range(self.component):
-            probi=0
-            for j,k,l in itertools.product(range(self.component), repeat=3):
-                probi+=prob[i][j][k][l]
-            print(probi)
-            pointenergy+=probi*np.log(probi)*T
-        for j in range(self.component):
-            probi=0
-            for i,k,l in itertools.product(range(self.component), repeat=3):
-                probi+=prob[i][j][k][l]
-            print(probi)
-            pointenergy+=probi*np.log(probi)*T
-        for k in range(self.component):
-            probi=0
-            for i,j,l in itertools.product(range(self.component), repeat=3):
-                probi+=prob[i][j][k][l]
-            print(probi)
-            pointenergy+=probi*np.log(probi)*T
-        for l in range(self.component):
-            probi=0
-            for i,j,k in itertools.product(range(self.component), repeat=3):
-                probi+=prob[i][j][k][l]
-            print(probi)
-            pointenergy+=probi*np.log(probi)*T
+        for i in range(self.clustersize):
+            for j in range(self.component):
+                position_type_matrix=np.zeros((2,1))
+                position_type_matrix[0][0]=i
+                position_type_matrix[1][0]=j
+                probi=utility.get_cluster_prob(prob,position_type_matrix)
+                pointenergy+=probi*np.log(probi)*T
         totalpotential=0
         for 
 
